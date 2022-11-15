2170f120e3afc8ad57cc152265d9e00214899bd8
Korakaas <anaismeynard@gmail.com>
2022-11-15 16:11:48 +0100

    nouveau component pastries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PastriesComponent } from './pastries/pastries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PastriesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pastries/pastries.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pastries',
+  templateUrl: './pastries.component.html',
+  styleUrls: ['./pastries.component.scss']
+})
+export class PastriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
