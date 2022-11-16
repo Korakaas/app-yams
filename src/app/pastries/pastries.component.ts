50f2882905dcc4b4830a46aeea3cb98f9b54f7be
Korakaas <anaismeynard@gmail.com>
2022-11-16 10:33:04 +0100

    Affichage liste pâtisseries

--- src/app/pastries/pastries.component.ts
+++ src/app/pastries/pastries.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { List, Pastrie } from '../pastrie';
+import { PASTRIES } from './mock-pastries';
+import {INGREDIENTS_LISTS} from './mock-pastries'
 
 @Component({
   selector: 'app-pastries',
@@ -8,7 +11,9 @@ import { Component, OnInit } from '@angular/core';
 export class PastriesComponent implements OnInit // Interface OnInit 
 {
   titlePage: string = "Page principale : Liste des pâtisseries";
-  
+  pastries: Pastrie[]|null = PASTRIES;
+  list: List[] = INGREDIENTS_LISTS;
+
   constructor() { }
 
   ngOnInit(): void // permet d'initialiser au montage du component
